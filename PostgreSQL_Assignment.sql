142c001002578f229f542bac7653a2a3d7cad89c
Sudipta Das <shiningsudipto@gmail.com>
2024-10-22 22:20:31 +0600

    students, courses and enrollment table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,26 @@
 -- Active: 1729360115145@@127.0.0.1@5432@university_db
+
+-- creating students table
+CREATE Table students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(150),
+    age INT,
+    email VARCHAR(100) UNIQUE,
+    frontend_mark INT,
+    backend_mark INT,
+    status VARCHAR(50)
+)
+
+-- creating courses table
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(200),
+    credits INT
+)
+
+-- creating enrollment table
+create table enrollment(
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INT REFERENCES students(student_id),
+    course_id INT REFERENCES courses(course_id)
+)
